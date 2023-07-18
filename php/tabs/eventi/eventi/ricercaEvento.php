cc115f8f13e17a0f45711c54207750fd189f0cb3
Kosta24 <jleon7221@gmail.com>
2023-07-18 13:17:05 +0200

    update Filtri1

--- php/tabs/eventi/eventi/ricercaEvento.php
+++ php/tabs/eventi/eventi/ricercaEvento.php
@@ -1,43 +1,47 @@
 <div id="filtri-calendar">
-  <h2>Filtri: </h2>
-  <div class="row">
-    <input class="col" type="text" id="eventRicerca_disciplina" placeholder="Disciplina">
-  </div>
-  <div class="row">
-    <input class="col" type="text" id="eventRicerca_attivita" placeholder="Attivita">
-  </div>
-  <div class="row">
-    <input class="col" type="text" id="eventRicerca_locale" placeholder="Locale">
-  </div>
-  <div class="row">
-    <select class="col" id="eventRicerca_ente" placeholder="ente">
-      <option>Ente</option>
-      <option>Ente1</option>
-      <option>Ente2</option>
-    </select>
-  </div>
-  <div class="row">
-    <select class="col" id="eventRicerca_stato" placeholder="stato">
-      <option>stato</option>
-      <option>1</option>
-      <option>2</option>
-    </select>
-    <label class="col" for="eventRicerca_annullato">Annullato:
-      <input class="form-check" style="margin:auto" type="checkbox" id="eventRicerca_annullato"
-        name="eventRicerca_annullato"></label>
-  </div>
+    <h2>Filtri: </h2>
+    <div class="input-holder">
+        <input class="float-label inner-label" type="text" id="eventRicerca_disciplina" name="eventRicerca_disciplina" required />
+        <label for="eventRicerca_disciplina">Disciplina</label>
+    </div>
+    <div class="input-holder">
+        <input class="float-label inner-label" type="text" id="eventRicerca_attivita" name="eventRicerca_attivita" required />
+        <label for="eventRicerca_attivita">Attivita</label>
+    </div>
+    <div class="input-holder">
+        <select class="float-label inner-label" type="text" id="eventRicerca_locale" name="eventRicerca_locale" required></select>
+        <label for="eventRicerca_locale">Locale</label>
+    </div>
+    <div class="input-holder">
+        <select class="float-label inner-label" type="text" id="eventRicerca_ente" name="eventRicerca_ente" required>
+            <option>Ente</option>
+            <option>Ente1</option>
+            <option>Ente2</option>
+        </select>
+        <label for="eventRicerca_ente">Ente</label>
+    </div>
+    <div class="row input-holder">
+        <select class="col float-label inner-label" type="text" id="eventRicerca_stato" name="eventRicerca_stato" required>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+        </select>
+        <label for="eventRicerca_stato">stato</label>
+        <label class="col" for="eventRicerca_annullato">Annullato:
+            <input class="form-check" style="margin:auto" type="checkbox" id="eventRicerca_annullato" name="eventRicerca_annullato"></label>
+    </div>
 
-  <div class="row">
-    <label class="col-sm-3" for="eventRicerca_annullato">Arbitro
-      <input class="form-check" style="margin:auto" type="checkbox" id="eventRicerca_annullato"  name="eventRicerca_annullato"></label>
+    <div class="row">
+        <label class="col-sm-3" for="eventRicerca_annullato">Arbitro
+            <input class="form-check" style="margin:auto" type="checkbox" id="eventRicerca_annullato" name="eventRicerca_annullato"></label>
 
-    <label class="col-sm-3" for="eventRicerca_pubblico">Pubblico
-      <input class="form-check" style="margin:auto" type="checkbox" id="eventRicerca_pubblico" name="eventRicerca_pubblico"></label>
+        <label class="col-sm-3" for="eventRicerca_pubblico">Pubblico
+            <input class="form-check" style="margin:auto" type="checkbox" id="eventRicerca_pubblico" name="eventRicerca_pubblico"></label>
 
-    <label class="col-sm-3" for="eventRicerca_RespSicurezza">RespSicurezza
-      <input class="form-check" style="margin:auto" type="checkbox" id="eventRicerca_RespSicurezza"name="eventRicerca_RespSicurezza"></label>
-  </div>
-  <div class="row">
-    <button class="closeOverlayBtn col" id="clearOverlayBtn">Pulisci Campi</button>
-  </div>
+        <label class="col-sm-3" for="eventRicerca_RespSicurezza">RespSicurezza
+            <input class="form-check" style="margin:auto" type="checkbox" id="eventRicerca_RespSicurezza" name="eventRicerca_RespSicurezza"></label>
+    </div>
+    <div class="row">
+        <button class="closeOverlayBtn col" id="clearOverlayBtn">Pulisci Campi</button>
+    </div>
 </div>
